7fe4f2884ee14d7239222ae4864221776d691b3a
Ry <OfficiallyRy@outlook.com>
2023-05-03 02:22:03 -0600

    forgot an s, classic

--- SharpPractice.client/src/env.js
+++ SharpPractice.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:7045' : ''
+export const baseURL = dev ? 'https://localhost:7045' : ''
 export const useSockets = false
 export const domain = 'theonetruery.us.auth0.com'
 export const clientId = 'akV13tVJ4Z0r96mr8BwmKaFGah40DVxm'
